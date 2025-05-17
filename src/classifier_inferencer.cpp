8c9becc82f6ddb37c82710bb21059214e307961e
johnson-magic <2460171714@qq.com>
2025-05-18 05:10:27 +0800

    [REFACTOR]: code refactor

--- src/classifier_inferencer.cpp
+++ src/classifier_inferencer.cpp
@@ -1,25 +1,26 @@
-
 #include "classifier_inferencer.h"
 
-void ClassifierInferencer::GetInputInfo(){
-    // get the input information
-	// 1. numInputNodes_
-	// 2. input_node_names_
-	// 3. input_w_ and input_h_
-
-	numInputNodes_ = GetSessionInputCount();
+void ClassifierInferencer::Init(const std::string &model_path){
+    static Ort::Env env(ORT_LOGGING_LEVEL_WARNING, "default");
     
+    Ort::SessionOptions option;
+    option.SetIntraOpNumThreads(1);
+    option.SetGraphOptimizationLevel(ORT_ENABLE_ALL);
+    session_ = new Ort::Session(env, ConvertToWString(model_path).c_str(), option);
+}
+
+void ClassifierInferencer::GetInputInfo(){
+	numInputNodes_ = GetSessionInputCount();
     for (int input_id = 0; input_id < numInputNodes_; input_id++) {
         Ort::AllocatorWithDefaultOptions allocator;  // 如何理解allocator的工作机制？ 它能够被复用吗？
         auto input_name = session_->GetInputNameAllocated(input_id, allocator);  // 通常，numInputNodes_只为1
         input_node_names_.push_back(input_name.get());  // char* -> string
 	
         Ort::TypeInfo input_type_info = session_->GetInputTypeInfo(input_id);
-	
         auto input_tensor_info = input_type_info.GetTensorTypeAndShapeInfo();
         auto input_dims = input_tensor_info.GetShape();
-        input_h_.push_back(input_dims[2]);
-        input_w_.push_back(input_dims[3]);    
+        net_h_.push_back(input_dims[2]);
+        net_w_.push_back(input_dims[3]);    
 	}
 }
 
@@ -32,14 +33,9 @@ size_t ClassifierInferencer::GetSessionOutputCount(){
 }
 
 void ClassifierInferencer::GetOutputInfo(){
-    // get the output information
-	// 1. numOutputNodes_
-	// 2. Output_node_names_
-	// 3. Output_w_ and input_h_
-
 	numOutputNodes_ = GetSessionOutputCount();
     
-    for(int output_id = 0; output_id < numInputNodes_; output_id++){
+    for(int output_id = 0; output_id < numOutputNodes_; output_id++){
         Ort::AllocatorWithDefaultOptions allocator;
         auto out_name = session_->GetOutputNameAllocated(output_id, allocator);
         output_node_names_.push_back(out_name.get());
@@ -47,24 +43,30 @@ void ClassifierInferencer::GetOutputInfo(){
         Ort::TypeInfo output_type_info = session_->GetOutputTypeInfo(output_id);
         auto output_tensor_info = output_type_info.GetTensorTypeAndShapeInfo();
         auto output_dims = output_tensor_info.GetShape();
-        
         // output_h_.push_back(output_dims[1]);  // 注意：对于检测模型，模型的输出一般为(B, 8, anchor_point)
         // output_w_.push_back(output_dims[2]);
-        output_class_num_.push_back(output_dims[1]);   
+        class_num_.push_back(output_dims[1]);   
     } 
 }
 
-void ClassifierInferencer::PreProcess(std::string& image_path){
-    image_path_ = return_image_path(image_path);
-	image_ = cv::imread(image_path_);
-	if (image_.empty()) {
-		std::cerr << "Failed to read the image!" << std::endl;
-		return;
-	}
+void ClassifierInferencer::PreProcess(const std::string& image_path){
+    image_path_ = image_path;
+    std::ifstream test_open(image_path_);
+    if(test_open.is_open()){
+        image_ = cv::imread(image_path_);
+        if (image_.empty()) {
+	    	std::cerr << "Failed to read the image!" << std::endl;
+            return;
+        }
+        test_open.close(); 
+    }else{
+        std::cerr << "Failed to read the image!" << std::endl;
+        return;
+    }
 	image_ = pad_and_resize(image_);
 }
 
-void ClassifierInferencer::PreProcess(cv::Mat& image){
+void ClassifierInferencer::PreProcess(cv::Mat image){
 	image_ = image;
 	if (image_.empty()) {
 		std::cerr << "Failed to read the image!" << std::endl;
@@ -108,20 +110,17 @@ void ClassifierInferencer::SaveOrtValueAsImage(Ort::Value& value, const std::str
     if (!cv::imwrite(filename, imageToSave)) {
         std::cerr << "Failed to save image to " << filename << std::endl;
 	}
-    // } else {
-    //     std::cout << "Image saved to " << filename << std::endl;
-    // }
 }
 
 void ClassifierInferencer::Inference(){
 	const std::array<const char*, 1> inputNames = { input_node_names_[0].c_str() };  // std::array用于fixed size array
 	const std::array<const char*, 1> outNames = { output_node_names_[0].c_str() };
    
-    	cv::Mat blob;
-	cv::dnn::blobFromImage(image_, blob, 1 / 255.0, cv::Size(input_w_[0], input_h_[0]), cv::Scalar(0, 0, 0), true, false);  // swapRB = true, crop = false,
+    cv::Mat blob;
+	cv::dnn::blobFromImage(image_, blob, 1 / 255.0, cv::Size(net_w_[0], net_h_[0]), cv::Scalar(0, 0, 0), true, false);  // swapRB = true, crop = false,
     
-	size_t tpixels = input_h_[0] * input_w_[0] * 3 * 1;
-	std::array<int64_t, 4> input_shape_info{ 1, 3, input_h_[0], input_w_[0]};
+	size_t tpixels = net_h_[0] * net_w_[0] * 3 * 1;
+	std::array<int64_t, 4> input_shape_info{ 1, 3, net_h_[0], net_w_[0]};
 
     
 	auto allocator_info = Ort::MemoryInfo::CreateCpu(OrtDeviceAllocator, OrtMemTypeCPU);
@@ -145,14 +144,13 @@ void ClassifierInferencer::Inference(){
 }
 
 void ClassifierInferencer::PostProcess(){
-	// output data
     predictions_.clear();
     scores_.clear();
 	const float* pdata = ort_outputs_[0].GetTensorMutableData<float>();
 	int prediction = 0;
     float max_prob = *(pdata + prediction);
     int begin_id = 1;
-    for(int i = begin_id; i<output_class_num_[0]; i++){
+    for(int i = begin_id; i<class_num_[0]; i++){
         if(*(pdata + i) > max_prob){
             max_prob = *(pdata + i);
             prediction = i;
@@ -166,37 +164,36 @@ void ClassifierInferencer::PostProcess(){
 
 }
 
-cv::Mat ClassifierInferencer::pad_and_resize(const cv::Mat img){
+cv::Mat ClassifierInferencer::pad_and_resize(const cv::Mat &img){
 	/*如果原始图片的宽和高有任意1个超过网络的输入宽、高，则先按照长宽比进行padding，再resize；
       否则，直接padding到网络的输入宽、高*/
     
     int w_img = img.cols;
     int h_img = img.rows;
     
-    
     // 计算缩放比例
-    scale_ = std::min(static_cast<double>(input_h_[0]) / h_img, static_cast<double>(input_w_[0]) / w_img);
-    scale_ = std::min(static_cast<double>(scale_), 1.0);  //如果原始图片的宽和高都不超过网络的宽和高，则将scale设置为1，表示事实上不进行resize.
+    float scale = std::min(static_cast<double>(net_h_[0]) / h_img, static_cast<double>(net_w_[0]) / w_img);
+    scale = std::min(static_cast<double>(scale), 1.0);  //如果原始图片的宽和高都不超过网络的宽和高，则将scale设置为1，表示事实上不进行resize.
     // 计算新的宽度和高度
-    int w_resized = static_cast<int>(std::round(scale_ * w_img));
-    int h_resized = static_cast<int>(std::round(scale_ * h_img));
+    int w_resized = static_cast<int>(std::round(scale * w_img));
+    int h_resized = static_cast<int>(std::round(scale * h_img));
     // 缩放图像
     cv::Mat img_resized;
     cv::resize(img, img_resized, cv::Size(w_resized, h_resized), 0, 0, cv::INTER_LINEAR);
 
     // 计算边框宽度和高度
-    int dw = input_w_[0] - w_resized;
-    int dh = input_h_[0] - h_resized;
+    int dw = net_w_[0] - w_resized;
+    int dh = net_h_[0] - h_resized;
 
     // 分配边框宽度（上下左右）
-    top_ = dh / 2;
-    bottom_ = dh - top_;
-    left_ = dw / 2;
-    right_ = dw - left_;
+    int top = dh / 2;
+    int bottom = dh - top;
+    int left = dw / 2;
+    int right = dw - left;
 
     // 添加边框
     cv::Mat img_bordered;
-    cv::copyMakeBorder(img_resized, img_bordered, top_, bottom_, left_, right_,
+    cv::copyMakeBorder(img_resized, img_bordered, top, bottom, left, right,
                        cv::BORDER_CONSTANT, cv::Scalar(0, 0, 0));
 
     // 返回处理后的图像
